49455e1212fb216e92b3a7f03caea4311fc5cd33
SachinK568 <84757638+SachinK568@users.noreply.github.com>
2024-08-26 17:51:59 +0530

    Update

---
+++ new/range1.py
@@ -0,0 +1,28 @@
+# for i in range (1,101):
+#     print(i)
+# for i in range(101,0,-1):
+#     print(i)
+
+# n = int(input("Enter_no:"))
+# for i in range(1, 11):
+#     print(n * i)
+
+# n = 5
+# sum = 0
+# i = 0
+# while i<=5:
+#     sum = sum + i
+#     i = i +1
+# print(sum)
+
+# n = 5
+# sum = 0
+# for i in range(n+1):
+#     sum = sum+i
+# print(sum)
+
+n = 5
+sum = 1
+for i in range(1,n+1):
+    sum = sum*i
+print(sum)
